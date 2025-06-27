32b4ca06f3ffcf316f52f2af54ab20d8f3403092
Marton Jusztin <jusztin.marton.menyhert@itk.ppke.hu>
2025-06-27 11:10:13 +0200

    Configurations for github pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://mjusztin.github.io',
 	integrations: [mdx(), sitemap()],
 	  redirects: {
     "/": "/blog",
